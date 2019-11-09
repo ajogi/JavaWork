8a5f9192282751cc2ce0a2f3eccd25b2eb9a9900
jogi <anagani6666@gmail.com>
2019-11-09 14:16:38 +0530

    new repo

---
+++ HCLExamples/Arith/Student.java
@@ -0,0 +1,15 @@
+package Arith;
+
+public class Student {
+	public String name;
+	public int rollno;
+	public void display() {
+		String title="ABC";
+		System.out.print(title);
+		System.out.println("Jogi");
+		title="Roll No.:";
+		System.out.print(title);
+		System.out.println("50");
+		
+	}
+}
---
+++ javafundamentals/HelloWorld.java
@@ -0,0 +1,9 @@
+package javafundamentals;
+
+public class HelloWorld {
+ public static void main(String[] args)
+{
+	 for(int i=0; i<5; i++)
+	System.out.println("\"Hello Jogi....\"\n siva"+ i);
+ }
+}
