4d66677ad337e69de9bd3a1a98b4cfbd9be697a2
jogi <anagani6666@gmail.com>
2019-12-18 16:20:21 +0530

    lastone

--- javafundamentals/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package javafundamentals;
-
-public class HelloWorld {
- public static void main(String[] args)
-{
-	 for(int i=0; i<5; i++)
-	System.out.println("\"Hello Jogi....\"\n siva"+ i);
- }
-}
